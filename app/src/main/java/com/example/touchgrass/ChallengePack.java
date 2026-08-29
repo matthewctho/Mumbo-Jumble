b23fd3fcf050a03b8f65fe2571fbe9ca01008bdf
Nikhil Subramani <nsub0954@uni.sydney.edu.au>
2026-08-29 19:31:57 +1000

    Initial commit for challenges section for backend

---
+++ app/src/main/java/com/example/touchgrass/ChallengePack.java
@@ -0,0 +1,13 @@
+package com.example.touchgrass;
+// This is the challenges description and activities
+public class ChallengePack {
+    public String name;
+    public String activityType;
+    public String description;
+
+    public ChallengePack(String name, String activityType, String description) {
+        this.name = name;
+        this.activityType = activityType;
+        this.description = description;
+    }
+}
---
+++ app/src/main/java/com/example/touchgrass/PackAdapter.java
@@ -0,0 +1,49 @@
+package com.example.touchgrass;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class PackAdapter extends RecyclerView.Adapter<PackAdapter.PackViewHolder> {
+
+    private List<ChallengePack> packList;
+
+    public PackAdapter(List<ChallengePack> packList) {
+        this.packList = packList;
+    }
+
+    @NonNull
+    @Override
+    public PackViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_pack, parent, false);
+        return new PackViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PackViewHolder holder, int position) {
+        ChallengePack pack = packList.get(position);
+        holder.txtPackName.setText(pack.name);
+        holder.txtPackDescription.setText(pack.description);
+    }
+
+    @Override
+    public int getItemCount() {
+        return packList.size();
+    }
+
+    static class PackViewHolder extends RecyclerView.ViewHolder {
+        TextView txtPackName;
+        TextView txtPackDescription;
+
+        public PackViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtPackName = itemView.findViewById(R.id.txtPackName);
+            txtPackDescription = itemView.findViewById(R.id.txtPackDescription);
+        }
+    }
+}
