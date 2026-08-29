b3be67243454be624a550c53106f0e26f8d87a05
Matthew Ho <maho0804@uni.sydney.edu.au>
2026-08-29 19:58:51 +1000

    Implementing first draft version of login system and Firebase

---
+++ app/src/main/java/com/example/touchgrass/data/AuthService.java
@@ -0,0 +1,34 @@
+package com.example.touchgrass.data;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class AuthService {
+
+    public interface AuthCallback {
+        void onSuccess(FirebaseUser user);
+        void onFailure(Exception e);
+    }
+
+    private final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+    public void signUp(String email, String password, AuthCallback callback) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener(result -> callback.onSuccess(result.getUser()))
+                .addOnFailureListener(callback::onFailure);
+    }
+
+    public void signIn(String email, String password, AuthCallback callback) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener(result -> callback.onSuccess(result.getUser()))
+                .addOnFailureListener(callback::onFailure);
+    }
+
+    public void signOut() {
+        firebaseAuth.signOut();
+    }
+
+    public FirebaseUser getCurrentUser() {
+        return firebaseAuth.getCurrentUser();
+    }
+}
---
+++ app/src/main/java/com/example/touchgrass/data/FirestoreService.java
@@ -0,0 +1,30 @@
+package com.example.touchgrass.data;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirestoreService {
+
+    public interface WriteCallback {
+        void onSuccess();
+        void onFailure(Exception e);
+    }
+
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public void createUserProfile(String uid, String displayName, WriteCallback callback) {
+        Map<String, Object> profile = new HashMap<>();
+        profile.put("displayName", displayName);
+        profile.put("points", 0);
+        profile.put("activePacks", new ArrayList<String>());
+        profile.put("pointsHidden", false);
+
+        db.collection("users").document(uid)
+                .set(profile)
+                .addOnSuccessListener(unused -> callback.onSuccess())
+                .addOnFailureListener(callback::onFailure);
+    }
+}
