3b218abe3bce4b23f4c6fe52cff37d929c9091b8
Nikhil Subramani <nsub0954@uni.sydney.edu.au>
2026-08-30 01:11:12 +1000

    modified the JSON file for the new database

--- app/src/main/java/com/mumble_jumble/touchgrass/activity/TaskListActivity.java
+++ app/src/main/java/com/mumble_jumble/touchgrass/activity/TaskListActivity.java
@@ -1,13 +1,13 @@
-package com.example.touchgrass.activity;
+package com.mumble_jumble.touchgrass.activity;
 
 import android.os.Bundle;
 import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import com.example.touchgrass.R;
-import com.example.touchgrass.adapters.TaskAdapter;
-import com.example.touchgrass.models.Task;
+import com.mumble_jumble.touchgrass.R;
+import com.mumble_jumble.touchgrass.adapters.TaskAdapter;
+import com.mumble_jumble.touchgrass.models.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import java.util.ArrayList;
